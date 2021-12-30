55eb6b0e568266c4c89f5740a7c6c7e982f4badb
Max William <83377057+maxwsj@users.noreply.github.com>
2021-12-30 20:04:21 -0300

    Simon Game - Implementação de novas funcionalidades e refatoração do código

--- jQuery/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ jQuery/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,92 +1,127 @@
-let buttonColours = ["red", "blue", "green", "yellow"];
+const TIME_ANIMATE_BUTTON = 100;
+const TIME_TO_ANIMATION = 200;
+const TIME_TO_NEW_SEQUENCE = 1000;
+const TIME_TO_SHOW_GAME_OVER = 200;
+const GAME_OVER_TEXT = "Game Over, Aperte qualquer tecla para reiniciar";
+
+const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
-
 let started = false;
 let level = 0;
+let timeBetweenSteps = 1000;
+let gameDificultyQuantity = 120;
 
+const gameDificulty = () => {
+  // Nega-se o resto da divisão para que não entre no aumento da dificuldade
+  if (!(level % 2)) {
+    timeBetweenSteps = timeBetweenSteps - gameDificultyQuantity;
+    if (!(level % 10)) {
+      gameDificultyQuantity = 50;
+    }
+  }
+};
+
+const nextLevel = () => {
+  // Contador do Level
+  level++;
+  $("#level-title").text("Level " + level);
+  gameDificulty();
+};
+
+const nextSequence = () => {
+  nextLevel();
+  // Variável para gerar o número aleatória e a cor aleatória
+  let randomNumber = Math.trunc(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  // Push para a pontuação atual do jogo
+  gamePattern.push(randomChosenColour);
+  userClickedPattern = [];
+};
 // Inicia o jogo
 $(document).on("keypress", function () {
   // Quando uma tecla do teclado é pressionada, a variável é alterada para true e inicia-se o if
   if (!started) {
-    $("#level-title").text("Level " + level);
     nextSequence();
+    showLastSequence();
     started = true;
   }
 });
 
-$(".btn").on("click", function (event) {
-  let userChosenColour = $(this).attr("id"); // Acessa
+// Função para gerar o som de acordo com a cor ao clicar em um botão
+const playSong = (name) => new Audio("sounds/" + name + ".mp3").play();
+
+// Função para gerar a animação ao clicar em um botão
+const animatePress = (currentColour) => {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, TIME_ANIMATE_BUTTON);
+};
+
+// Função para resetar o Jogo
+const startOver = () => {
+  level = 0;
+  gamePattern = [];
+  started = false;
+};
+
+// Função para checar o resultado
+const checkAnswer = (currentLevel) => {
+  if (gamePattern.length === userClickedPattern.length) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      // Inicia outra sequencia após o tempo do último clique
+      setTimeout(() => {
+        showLastSequence();
+        nextSequence();
+      }, TIME_TO_NEW_SEQUENCE);
+    } else {
+      $("body").addClass("game-over"); // Adiciona ao corpo a classe css game-over
+      setTimeout(() => {
+        $("body").removeClass("game-over"); // Remove ao corpo a classe css game-over
+      }, TIME_TO_SHOW_GAME_OVER);
+      playSong("wrong"); // Executa o som caso a sequencia esteja errada
+      $("#level-title").text(GAME_OVER_TEXT);
+      startOver();
+    }
+  }
+};
+
+$(".btn").on("click", function () {
+  let userChosenColour = $(this).attr("id");
   playSong(userChosenColour);
   animatePress(userChosenColour);
   userClickedPattern.push(userChosenColour); // armazena os cliques do usuário
   checkAnswer(userClickedPattern.length - 1); // Obtem a última resposta contida na array, do clique do usuário
 });
 
-function nextSequence() {
-  // Contador do Level
-  level++;
-  $("#level-title").text("Level " + level);
+const step = (thisStep) => {
+  playSong(thisStep);
+  $("#" + thisStep)
+    .fadeIn(TIME_TO_ANIMATION)
+    .fadeOut(TIME_TO_ANIMATION)
+    .fadeIn(TIME_TO_ANIMATION);
+};
 
-  // Variável para gerar o número aleatória e a cor aleatória
-  let randomNumber = Math.trunc(Math.random() * 4);
-  let randomChosenColour = buttonColours[randomNumber];
+const repeatStep = (itemStep) => {
+  // Mostrar um passo
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Verificar se o passo existe
+      if (itemStep) {
+        step(itemStep);
+        resolve("repetido com sucesso");
+      } else {
+        reject(Error("não existe esse passo")); // Retorna um erro
+      }
+    }, timeBetweenSteps);
+  });
+};
 
-  // Animação aleatória de acordo com a cor
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
-  // Função para tocar o som referente a cor
-  playSong(randomChosenColour);
-
-  // Push para a pontuação atual do jogo
-  gamePattern.push(randomChosenColour);
-
-  // Resetando a array (para que os pushs de cliques do jogador sejam inseridos novamente e compaarados com a array gamePattern)
-  userClickedPattern = [];
-}
-
-// Função para gerar o som de acordo com a cor ao clicar em um botão
-function playSong(name) {
-  let audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-// Função para gerar a animação ao clicar em um botão
-function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");
-  }, 100);
-}
-
-// Função para checar o resultado
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("Success!");
-    // Verificar se o usuário já terminou de inserir as sequencias
-    if (gamePattern.length === userClickedPattern.length) {
-      // Inicia outra sequencia após o tempo do último clique
-      setTimeout(function () {
-        nextSequence();
-      }, 1000);
-    }
-  } else {
-    $("body").addClass("game-over"); // Adiciona ao corpo a classe css game-over
-    setTimeout(function () {
-      $("body").removeClass("game-over"); // Remove ao corpo a classe css game-over
-    }, 200);
-    playSong("wrong"); // Executa o som caso a sequencia esteja errada
-    $("#level-title").text("Game Over, Aperte qualquer tecla para reiniciar");
-    console.log("Wrong!");
-    startOver();
+async function showLastSequence() {
+  for (const pattern of gamePattern) {
+    await repeatStep(pattern)
+      .then((value) => console.log(`passo ${pattern} ${value}`)) // Quando é realizado com sucesso
+      .catch((value) => console.log(`passo ${pattern} ${value}`)); // Quando não é realizado com sucesso
   }
-}
-
-// Função para resetar o Jogo
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
 }
